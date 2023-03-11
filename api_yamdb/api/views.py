fe9bdc109a8a5fa57389c067c59b333e5bda6465
spechaly <baluew.1998@mail.ru>
2023-03-11 22:17:52 +0300

    jp

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
 from django.db.models import Avg
 from django.shortcuts import get_object_or_404
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import AllowAny
@@ -23,7 +22,6 @@ class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.annotate(
         rating=Avg('reviews__score')).order_by('name')
     permission_classes = (permissions.IsAdminOrReadOnly, )
-    filter_backends = (DjangoFilterBackend,)
     filterset_class = TitleFilter
 
     def get_serializer_class(self):
